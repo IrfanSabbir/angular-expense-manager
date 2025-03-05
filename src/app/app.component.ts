bba514661b5bf1d65e3638efa4acaa7becfe2568
IrfanSabbir <avoidirfan@gmail.com>
2025-03-05 10:58:02 +0600

    Basic expense manager setup with angular-ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [HomeComponent, HeaderComponent],
+  template: `
+    <app-header />
+    <app-home />
+  `,
+  styles: [
+    `
+      main {
+        padding: 16px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'expense-manager';
