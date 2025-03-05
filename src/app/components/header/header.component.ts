bf9a50731b783f53c483d6cb9319c7848b88cada
IrfanSabbir <avoidirfan@gmail.com>
2025-03-05 12:16:24 +0600

    Routing setup with outlet and link with SPA's

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  title = signal('Angular app');
+
+  mainPage = {title: 'Angular expense monitor', path: '/'};
+  todoPage = {title: 'Todos', path: '/todos'};
 }
